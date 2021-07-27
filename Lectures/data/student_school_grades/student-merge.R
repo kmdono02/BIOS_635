5d976dd9da52249c42be025cbc3b2207f352003d
Kevin Donovan <43554362+kmdono02@users.noreply.github.com>
2021-07-27 15:54:13 -0700

    Delete Lectures directory

--- Lectures/data/student_school_grades/student-merge.R
+++
@@ -1,6 +0,0 @@
-d1=read.csv("../data/student_school_grades/student-mat.csv",header=TRUE) 
-d2=read.csv("../data/student_school_grades/student-por.csv",header=TRUE)
-
-d3=merge(d1,d2)
-print(nrow(d3)) # 382 students
-write.csv(d3, file="../data/student_school_grades/student_merge.csv")
